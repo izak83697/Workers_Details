65b63b9024ee944d94bd2f04ca9ba3b48c3da115
izak83697 <113294102+izak83697@users.noreply.github.com>
2022-12-29 16:16:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { AddWorkers } from "./components/AddWorkers";
 import { WorkersDetails } from "./components/WorkersDetails";
 import { ArraySorting } from "./components/ArraySorting";
 function App() {
-  const [arrWorkers, setArrWorkers] = useState(JSON.parse(localStorage.getItem("arrWorkers")));
+  let arr = localStorage.getItem("arrWorkers");
+  const [arrWorkers, setArrWorkers] = useState(
+    arr !== undefined && arr !== undefined && JSON.parse(arr)
+  );
 //saves all workers in localstorage so that even when the page is refreshed, the list of workers will not deleted
   useEffect(() => {
     localStorage.setItem("arrWorkers", JSON.stringify(arrWorkers));
